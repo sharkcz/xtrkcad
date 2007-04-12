31a25cf857c89059d8dbb032724554f6348b8502
m_fischer <none@none>
2007-04-12 05:33:37 +0000

    Setting version number to 4.0.2b4
    --HG--
    extra : convert_revision : svn%3Aad96d65e-77c6-4fb4-b022-7d5d8cc5d1e0/trunk%4086

--- app/bin/version.h
+++ app/bin/version.h
@@ -1,4 +1,4 @@
-/* $Header: /home/dmarkle/xtrkcad-fork-cvs/xtrkcad/app/bin/version.h,v 1.3 2007-02-11 12:33:18 m_fischer Exp $
+/* $Header: /home/dmarkle/xtrkcad-fork-cvs/xtrkcad/app/bin/version.h,v 1.4 2007-04-12 05:33:37 m_fischer Exp $
  */
 
 /*  XTrkCad - Model Railroad CAD
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
 
-#define VERSION "4.0.1"
+#define VERSION "4.0.2b1"
 #define PARAMVERSION (10)
 #define PARAMVERSIONVERSION "3.0.0"
 #define MINPARAMVERSION (1)
