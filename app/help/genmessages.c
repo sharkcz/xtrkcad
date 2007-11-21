0f13567c55ed75fd270050be1472e0ab5b007a6b
m_fischer <none@none>
2007-11-21 19:09:14 +0000

    Changed build process for new help files
    --HG--
    extra : convert_revision : svn%3Aad96d65e-77c6-4fb4-b022-7d5d8cc5d1e0/trunk%40189

--- app/help/genmessages.c
+++ app/help/genmessages.c
@@ -18,14 +18,11 @@
 
 #include <stdio.h>
 #include <string.h>
-#ifdef WINDOWS
 #include <stdlib.h>
+#ifdef WINDOWS
 	#if _MSC_VER >=1400
 		#define strdup _strdup
 	#endif
-#endif
-#if defined (__sun) && defined (__SVR4)
-#include <stdlib.h>
 #endif
 
 
@@ -38,6 +35,69 @@ typedef struct helpMsg_t {
 
 helpMsg_t helpMsgs[200];
 int helpMsgCnt = 0;
+
+struct transTbl {
+      char *inChar;
+      char *outChar[];
+};
+
+/* ATTENTION: make sure that the characters are in the same order as the equivalent escape sequences below */
+
+/* translation table for unicode sequences understood by Halibut */
+struct transTbl toUnicode = {
+       "°\0",
+      { "\\u00B0",
+		  "\\0"  }
+};
+
+/* translation table for escape sequences understood by C compiler */
+
+struct transTbl toC = {
+      "\\\"\0",
+      { "\\\\",
+      "\\\"",
+		"\\0" }
+};
+
+
+char *
+translateString( char *srcString, struct transTbl *trTbl )
+{
+	char *destString;
+   char *cp;
+   char *cp2;
+   int bufLen = strlen( srcString ) + 1;
+   char *idx;
+
+   /* calculate the expected result length */
+   for( cp = srcString; *cp; cp++ )
+   	if( idx = strchr( trTbl->inChar, *cp ))         /* does character need translation ? */
+      	bufLen += strlen( (trTbl->outChar)[idx - trTbl->inChar ] ) - 1; /* yes, extend buffer accordingly */
+
+   /* allocate memory for result */
+   destString = malloc( bufLen );
+
+   if( destString ) {
+		/* copy and translate characters as needed */
+      cp2 = destString;
+      for( cp = srcString; *cp; cp++ ) {
+      	if( idx = strchr( trTbl->inChar, *cp )) {       /* does character need translation ? */
+         	strcpy( cp2, (trTbl->outChar)[idx - trTbl->inChar ] );   /* yes, copy the escaped character sequence */
+            cp2 += strlen((trTbl->outChar)[idx - trTbl->inChar ] );
+         } else {
+            *cp2++ = *cp;                       /* no, just copy the character */
+         }
+		}
+   	/* terminate string */
+   	*cp2 = '\0';
+   } else {
+   	/* memory allocation failed */
+      exit(1);
+   }
+
+   return( destString );
+}
+
 
 int cmpHelpMsg( const void * a, const void * b )
 {
@@ -55,55 +115,128 @@ void unescapeString( FILE * f, char * str )
 	}
 }
 
+/**
+ * Generate the file in help source format ( ie. the BUT file )
+ */
 
-void dumpHelp( void )
+void dumpHelp( FILE *hlpsrcF )
 {
-	FILE * hlpsrcF;
 	int inx;
+	char *transStr;
+
+	/* sort in alphabetical order */
 	qsort( helpMsgs, helpMsgCnt, sizeof helpMsgs[0], cmpHelpMsg );
-	hlpsrcF = fopen( "messages.hlpsrc", "w" );
-	fprintf( hlpsrcF, "?LS\n" );
+
+	/* write the start of the help source */
+	fprintf( hlpsrcF, "\\A{messages} Error Messages\n\n" );
+
+	/* now save all the help messages */
 	for ( inx=0; inx<helpMsgCnt; inx++ ) {
-		fprintf( hlpsrcF, "?LI\n$B" );
-		unescapeString( hlpsrcF, helpMsgs[inx].title );
-		fprintf( hlpsrcF, "$\n?H" );
+	
+		transStr = translateString( helpMsgs[inx].title, &toUnicode );
+		fprintf( hlpsrcF, "\\H{%s} %s\n\n", helpMsgs[inx].key, transStr );
+		free( transStr );
+		
+		transStr = translateString( helpMsgs[inx].help, &toUnicode );
+		fprintf( hlpsrcF, "%s\n\n", transStr );
+		free( transStr );		
+#ifdef LATER
+		fprintf( hlpsrcF, "\\H{" );
 		unescapeString ( hlpsrcF, helpMsgs[inx].key );
+		fprintf( hlpsrcF, "} " );
+
+		unescapeString( hlpsrcF, helpMsgs[inx].title );
 		fprintf( hlpsrcF, "\n\n" );
+
 		unescapeString ( hlpsrcF, helpMsgs[inx].help );
-		fprintf( hlpsrcF, "\n\n?H****\n" );
+
+		fprintf( hlpsrcF, "\n\n" );
+#endif
 	}
-	fprintf( hlpsrcF, "?LE\n" );
-	fclose( hlpsrcF );
 }
 
 
 int main( int argc, char * argv[] )
 {
 	FILE * hdrF;
+	FILE *inF;
+	FILE *outF;
+	
 	char buff[256];
 	char * cp;
+	int inFileIdx;
 	enum {m_init, m_title, m_alt, m_help } mode = m_init;
 	char msgName[256];
 	char msgAlt[256];
 	char msgTitle[1024];
 	char msgTitle1[1024];
 	char msgHelp[4096];
-	int flags;
-
+#ifndef FLAGS	
+	int flags; 
+#endif
+	/* check argument count */
+	if ( argc < 3 || argc > 4 ) {
+		fprintf( stderr, "Usage: %s [-i18n] INFILE OUTFILE\n\n", argv[0] );
+		fprintf( stderr, "       -i18n is used to generate a include file with gettext support.\n\n" );
+		exit(1);
+	}
+	
+	/* check options */
+	if( argc == 4 ) {
+		if( !strcmp(argv[ 1 ], "-i18n")){
+			fprintf( stderr, "Option is not supported yet!\n" );
+			exit( 1 );
+		}	
+		/* inFileIdx = 2;  skip over option argument */
+	} else {
+		inFileIdx = 1;	/* first argument is input file */
+	}	
+	
+	/* open the file for reading */
+	inF = fopen( argv[ inFileIdx ], "r" );
+	if( !inF ) {
+			fprintf( stderr, "Could not open %s for reading!\n", argv[ inFileIdx ] );
+			exit( 1 );
+	}		
+			
+	/* open the include file to generate */		
 	hdrF = fopen( "messages.h", "w" );
-	while ( fgets( buff, sizeof buff, stdin ) ) {
+	if( !hdrF ) {
+			fprintf( stderr, "Could not open messages.h for writing!\n" );
+			exit( 1 );
+	}		
+	
+	/* open the help file to generate */
+	outF = fopen( argv[ inFileIdx + 1 ], "w" );
+	if( !inF ) {
+			fprintf( stderr, "Could not open %s for writing!\n", argv[ inFileIdx ] );
+			exit( 1 );
+	}		
+							
+	while ( fgets( buff, sizeof buff, inF ) ) {
+	
+		/* skip comment lines */
 		if ( buff[0] == '#' )
 			continue;
+			
+		/* remove trailing newline */	
 		cp = buff+strlen(buff)-1;
 		if (*cp=='\n') *cp = 0;
+		
+		
 		if ( strncmp( buff, "MESSAGE ", 8 ) == 0 ) {
+
+			/* skip any spaces */
 			cp = strchr( buff+8, ' ' );
 			if (cp)
 				while (*cp == ' ') *cp++ = 0;
+#ifndef FLAGS		
 			if ( cp && *cp ) {
 				flags = atoi(cp);
-			}
-			strcpy( msgName, buff+8 );
+			} 
+#endif			
+			/* save the name of the message */
+			strcpy( msgName, buff + 8 );
 			msgAlt[0] = 0;
 			msgTitle[0] = 0;
 			msgTitle1[0] = 0;
@@ -114,13 +247,19 @@ int main( int argc, char * argv[] )
 		} else if ( strncmp( buff, "HELP", 4 ) == 0 ) {
 			mode = m_help;
 		} else if ( strncmp( buff, "END", 3 ) == 0 ) {
+			/* the whole message has been read */
 			if (msgHelp[0]==0) {
+				/* no help text is included */
 				fprintf( hdrF, "#define %s \"%s\"\n", msgName, msgTitle );
 			} else if (msgAlt[0]) {
+				/* a help text and an alternate description are included */
 				fprintf( hdrF, "#define %s \"%s\\t%s\\t%s\"\n", msgName, msgName, msgAlt, msgTitle );
 			} else {
+				/* a help text but no alternate description are included */
 				fprintf( hdrF, "#define %s \"%s\\t%s\"\n", msgName, msgName, msgTitle );
 			}
+			
+			/* save the help text for later use */
 			if (msgHelp[0]) {
 				helpMsgs[helpMsgCnt].key = strdup(msgName);
 				if ( msgAlt[0] )
@@ -132,25 +271,36 @@ int main( int argc, char * argv[] )
 			}
 			mode = 0;
 		} else {
+			/* are we currently reading the message text? */
 			if (mode == m_title) {
+				/* yes, is the message text split over two lines ? */
 				if (msgTitle[0]) {
+					/* if yes, keep the first part as the short text */			
 					if (msgAlt[0] == 0) {
 						strcpy( msgAlt, msgTitle );
 						strcat( msgAlt, "..." );
 					}
+					/* add a newline to the first part */
 					strcat( msgTitle, "\\n" );
 				}
+				/* now save the buffer into the message title */
 				strcat( msgTitle, buff );
 			} else if (mode == m_alt) {
+				/* an alternate text was explicitly specified, save */
 				strcpy( msgAlt, buff );
 			} else if (mode == m_help) {
+				/* we are reading the help text, save in buffer */
 				strcat( msgHelp, buff );
 				strcat( msgHelp, "\n" );
 			}
 		}
 	}
+	dumpHelp( outF );
+
 	fclose( hdrF );
-	dumpHelp();
+	fclose( inF );
+	fclose( outF );
+	
 	printf( "%d messages\n", helpMsgCnt );
 	return 0;
 }
