d4b5a7ea49c9c22b41370f6699a00aee3b3701a4
m_fischer <none@none>
2008-03-09 18:10:34 +0000

    create param files from structure definitions
    --HG--
    extra : convert_revision : svn%3Aad96d65e-77c6-4fb4-b022-7d5d8cc5d1e0/trunk%40329

--- app/lib/params/mkstruct.c
+++ app/lib/params/mkstruct.c
@@ -1,7 +1,36 @@
+/** \file mkstruct.c
+ * Build utility to create simple rectangular structure definitions from a data file. 
+ *
+ * $Header: /home/dmarkle/xtrkcad-fork-cvs/xtrkcad/app/lib/params/mkstruct.c,v 1.2 2008-03-09 18:10:34 m_fischer Exp $
+ */
+
+/*  XTrkCad - Model Railroad CAD
+ *  Copyright (C) 
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#ifndef WINDOWS
 #include <string.h>
+
+#define BUFSIZE 1024
+
+#if _MSC_VER > 1300
+#define stricmp _stricmp
+#define strnicmp _strnicmp
 #endif
 
 int main ( int argc, char * argv [] )
@@ -10,40 +39,113 @@ int main ( int argc, char * argv [] )
 	double x;
 	double y;
 	int cm = 0;
+	char *inFile;
+	char *outFile;
+	FILE *fIn, *fOut;
+	int count = 0;
+	char *buffer = malloc( BUFSIZE );
+	char *desc;
+	char *p1;
+	char *p2 = malloc( BUFSIZE );
+	char *scale;
+	char *ptr;
+	int err;
 
-	while (argc > 1 && argv[1][0] == '-' ) {
-	    if ( strcmp( argv[1], "-cm" ) == 0 ) {
-		cm++;
-		argv++;
-		argc--;
-	    } else if ( strncmp( argv[1], "-color=", strlen( "-color=" ) ) == 0 ) {
-		color = strtol( argv[1]+strlen( "-color=" ), NULL, 16 );
-		argv++;
-		argc--;
-	    } else {
-		fprintf( stderr, "Unrecognized option: %s\n", argv[1] );
-		exit(1);
-	    }
+	if( argc != 3 )
+	{
+		fprintf( stderr, "Usage: mkstruct definitions.data param file\n\n"
+						 "The data file is read line by line and structure defimitions\n"
+						 "are created in the param file.\n\n"
+						 "The file structure is:\n"
+						 "scale \"description of structure\" x y [cm] [color=#rrggbb]\n\n"
+						 "scale                    : scale of structure\n"
+						 "description of structure : name, enclosed in double quotes\n"
+						 "x                        : x dimension of structure\n"
+						 "y                        : y dimension of structure\n"
+						 "cm                       : dimensions are in centimeters (default: inch) (opt.)\n"
+						 "color=#rrggbb            : color to use for structure (default: #FF00FF) (opt.)\n" );
+		exit( 1 );
 	}
 
-	if ( argc != 5 ) {
-		fprintf( stderr, "Usage: mkstruct [-cm] SCALE NAME X Y\n" );
-		exit(1);
+	inFile = malloc( strlen( argv[ 1 ] ));
+	strcpy( inFile, argv[ 1 ] );
+
+	outFile = malloc( strlen( argv[ 2 ] ));	
+	strcpy( outFile, argv[ 2 ] );
+
+//	if( ptr = strrchr( outFile, '.' ))				/* do we already have an extension? */
+//		strcpy( ptr, ".xtp" );						/* if yes, replace with xtp */
+//	else
+//		strcat( outFile, ".xtp" );					/* if no, add xtp */
+
+	fIn = fopen( inFile, "r" );
+	if( !fIn ) {
+		fprintf( stderr, "Could not open the definition %s\n", inFile );
+		exit( 1 );
 	}
 
-	x = atof( argv[4] );
-	y = atof( argv[3] );
-	if (cm) {
-		x /= 2.54;
-		y /= 2.54;
+	fOut = fopen( outFile, "w" );
+	if( !fOut ) {
+		fprintf( stderr, "Could not create the structures in %s\n", outFile );
+		exit( 1 );
 	}
 
-	printf("STRUCTURE %s \"%s\"\n", argv[1], argv[2] );
-	printf("\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, 0.0, 0.0, 0.0, x );
-	printf("\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, 0.0, x, y, x );
-	printf("\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, y, x, y, 0.0 );
-	printf("\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, y, 0.0, 0.0, 0.0 );
-	printf("\tEND\n");
+	if( fgets( buffer, BUFSIZE, fIn ))
+		fputs( buffer, fOut );
+	
+	while(fgets(buffer, BUFSIZE, fIn ))
+	{
+		err = 0;
+		scale = strtok( buffer, " \"" );
+		desc = strtok( NULL, "\"" );
 
+		if( scale == NULL && desc == NULL )
+			err = 1;
+
+		/* get the size information */
+		x = atof( strtok( NULL, " " ));
+		y = atof( strtok( NULL, " " ));
+
+		if( x == 0 || y == 0 ) {
+			err = 1;
+		}
+		/* try to get the next token */
+		p1 = strtok( NULL, " \r\n\t" );
+
+		/* we have an additional token, check it */
+		if( p1 ) {
+			ptr = strtok( NULL, " \r\n\t" );
+			if( !stricmp( p1, "cm" )) {
+				x /= 2.54;
+				y /= 2.54;
+				p1 = ptr;
+			}
+		} else {
+			p1 = strtok( NULL, " " );
+		}
+
+		if( p1 && !strnicmp( p1, "color=", strlen( "color=" ))) {
+			color = atoi( p1 + strlen( "color=#" ));
+		}
+
+		if( !err ) {
+			fprintf( fOut, "STRUCTURE %s \"%s\"\n", scale, desc );
+
+			fprintf( fOut, "\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, 0.0, 0.0, 0.0, x );
+			fprintf( fOut, "\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, 0.0, x, y, x );
+			fprintf( fOut, "\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, y, x, y, 0.0 );
+			fprintf( fOut, "\tL %ld 0 %0.6f %0.6f %0.6f %0.6f\n", color, y, 0.0, 0.0, 0.0 );
+			fprintf( fOut, "\tEND\n");
+		} else {
+			fprintf( stderr, "Error in line %d\n", count );
+			exit( 1 );
+		}
+
+		count++;
+	}
+
+	printf( "Created %d structures.\n", count );
+	fclose( fIn );
+	fclose( fOut );
 	exit(0);
 }
